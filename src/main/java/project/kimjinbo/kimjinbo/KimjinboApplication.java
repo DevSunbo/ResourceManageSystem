4aa8cdfaccd3fedd6495118bcdc349cd261f164b
taehu <asdnm5030@naver.com>
2020-09-20 23:12:18 +0900

    first commit. create spring boot project

---
+++ src/main/java/project/kimjinbo/kimjinbo/KimjinboApplication.java
@@ -0,0 +1,13 @@
+package project.kimjinbo.kimjinbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimjinboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimjinboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/kimjinbo/kimjinbo/KimjinboApplicationTests.java
@@ -0,0 +1,13 @@
+package project.kimjinbo.kimjinbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimjinboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
